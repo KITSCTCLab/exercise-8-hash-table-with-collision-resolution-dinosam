270acbe1bd667f8ae062fdf2360d2530913a6151
DINO <109952555+dinosam@users.noreply.github.com>
2022-09-24 18:13:48 +0530

    INITIAL COMMIT

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ def insert(Hashtable, keyvalue, value) -> None:
 
 # Do not edit the following code
 hash_table_size = int(input())
-# Create Hashtable as a list of list.
+# Create Hashtaist of list.
 HashTable = [[] for _ in range(hash_table_size)]
 input_data = input()
 data = []
